41cca704e17de6337737bf5af3702883781bd0de
Bulbul Yergalikyzy Kasymbek <b_kasymbek@kbtu.kz>
2023-02-16 12:18:51 +0600

    lab3

---
+++ lab3/JavaScript Info/variables.js
@@ -0,0 +1,11 @@
+let admin;
+var name;
+name="John";
+admin=name;
+alert(admin);
+
+let earth;
+let Moldir;
+
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+const age = someCode(BIRTHDAY); // make age uppercase?
