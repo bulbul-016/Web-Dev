acfac33371b63e49461b1421ffaf63ef767f7f2f
Bulbul Yergalikyzy Kasymbek <b_kasymbek@kbtu.kz>
2023-02-19 22:23:07 +0600

    lecture4

---
+++ Lab4/lecture4/2.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+var a;
+a = 2;
+// a = 'hello'; // error
+console.log(a);
+var user1 = {
+    name: 'User 1',
+    age: 20
+};
+console.log(user1.name);
+// console.log(user1.address); error
+/*
+C++
+int sum(int n1, int n2){
+    return n1 + n2;
+}
+*/
+function sum(n1, n2) {
+    return n1 + n2;
+}
+console.log(sum(2, 3));
+function greeting(user) {
+    console.log("Hi ".concat(user.name));
+}
+greeting(user1);
